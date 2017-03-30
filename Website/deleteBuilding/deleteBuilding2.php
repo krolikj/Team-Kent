182fe960668ecc3ec9328746e668eb1464e50360
yasiroh <aljarooy@mail.gvsu.edu>
2017-03-30 02:22:20 -0700

    Update deleteBuilding2.php

--- Website/deleteBuilding/deleteBuilding2.php
+++ Website/deleteBuilding/deleteBuilding2.php
@@ -7,12 +7,32 @@
 	
 	// Try and connect to the database
 	$conn = mysqli_connect($host,$user,$passward,$db);
-	
-	$answer = $_POST["answer"];
 
-	$sql = "DELETE FROM buildings where name = '$answer'";
+	$answer = $_POST["answer"];
+	
+	$sql = "DELETE FROM buildings where next = '$answer'";
 	if ($conn->query($sql) === TRUE) {
  		echo "record successfully deleted";
+
+		
+
+		$sql = "SELECT count(*) from answers where next >= $answer";
+		$results = mysqli_query($conn,$sql);
+		$ch[0] =0;
+		while($row2=mysqli_fetch_array($results)) {	  
+			$ch = $row2;
+		}
+		$count = 1;
+		$count2 = 0;
+		if($ch[0] >0){
+			
+			while($count <=$ch[0]){
+				$sql = "update answers set next=$answer+$count2 where next=$answer+$count";
+				$results = mysqli_query($conn,$sql);
+				$count++;
+				$count2++;
+			}
+		}
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
