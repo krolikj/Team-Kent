4552dc328ce10f9047b1899314495de1047c2bc6
yasiroh <aljarooy@mail.gvsu.edu>
2017-04-01 19:46:45 -0400

    Update loginCheck.php

--- Website/login/loginCheck.php
+++ Website/login/loginCheck.php
@@ -1,10 +1,7 @@
 <?php   
 
 	//Database info
-	$host = 'localhost';
-	$user = 'guest';
-	$passward = '123456';
-	$db = 'studint_services';
+	include('databaseInfo.php');
 
 	// Try and connect to the database
 	$conn = mysqli_connect($host,$user,$passward,$db);
