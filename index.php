6e1b395cbf5ce4a54e2c2d2254eb733a3bbf1d0d
Bryana Craig <craigb@mail.gvsu.edu>
2017-04-09 17:30:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,182 @@
+<html>
+<head>
+<?php header("Cache-Control: no-cache, must-revalidate");?>
+<title>Capstone 467 Winter 2017 - GV Student Helper</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='custom.css' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+<header class="clearfix">
+	<div class="header-left">
+		<h1>Capstone 467 Winter 2017 - GV Student Helper</h1>
+	</div>
+	<div class="header-right">
+		<label for="open">
+			<span class="hidden-desktop" style="left: -19px; top: 1px; height: 2em"></span>
+		</label>
+		<input type="checkbox" name="" id="open">
+			<nav>
+				<?php include('login/loginCheck.php'); ?>
+			</nav>
+		</div>
+		</header>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-lg-offset-2">
+      
+	
+<style>
+#qTable td {
+  padding: 5px 1em;
+  border-radius: 5px;
+  background-color: #eeefff;
+  font-family: "Segoe UI"
+}
+
+#qTable {
+  width: 100%;
+  border-spacing: 0.5em
+}
+
+#qTable li {
+  cursor: pointer
+}
+
+#qTable li:hover {
+  text-decoration: underline
+}
+
+#qTable tr:nth-child(n+2) {
+  display: none
+}
+
+#qTable p {
+  font-weight: bold;
+  line-height: 100%
+}
+
+#errdiv {
+  display: none;
+  font-weight: bold;
+  color: #903;
+  padding: 0.3em
+}
+
+.selectedAnswer {
+  font-weight: bold;
+  color: #0d0f3a
+}
+
+i {
+  display: none
+}
+
+button { 
+  background:none;
+  border:none; 
+}
+header{
+	width: 100%;
+	height: auto;
+	background: #2a2a44;
+}
+.header-left,
+.header-right{
+	width: 100%;
+	position: relative;
+	text-align: center;
+	color: white;
+}
+
+.header-right label{
+	position: absolute;
+	top: -3.7em;
+	right: 0;
+	cursor: pointer;
+}
+.header-right span{
+	position: relative;
+	width: 2em;
+	height: 2em;
+	background: white;
+	
+}
+.header-right span:hover{
+	background: #b2b1d3;
+}
+.header-right span:before,
+.header-right span:after{
+	content: '';
+	position: absolute;
+	width: 2em;
+	height: .5em;
+	top: 4px;
+	left: 0;
+	background: #2a2a44;
+}
+.header-right span:after{
+	top: 16px;
+}
+
+#open{
+	display: none;
+}
+
+.hidden-desktop{
+	display: block;
+}
+
+nav{
+	line-height: 40px;
+	height: 0;
+	overflow: hidden;
+	position:relative;
+	display:inline-block
+}
+
+nav>a{
+	color: white;
+	padding: 0 1em;
+	display: block;
+}
+nav>a:hover{
+	background: #eeefff;
+	color: #2a2a44;
+}
+</style>
+
+      
+
+      <div id="errdiv"></div>
+	<br><br><br>
+      <table id="qTable">
+      
+  	<?php include('backend.php'); ?>
+
+      </table>
+    </div>
+    <!-- /.8 --> 
+    
+  </div>
+  <!-- /.row--> 
+  
+</div>
+<!-- /.container-->
+
+<div id="map"></div>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBItn1cFWKbaAIFdY2XKr-jD_bqeWB-yRs" type="text/javascript"></script> 
+<script type="text/javascript"> var buildingsInfo =<?php echo json_encode($buildings); ?>; </script>
+<script type="text/javascript"> var name[] =<?php echo json_encode($name); ?>; </script>
+<script type="text/javascript"> var lat[] =<?php echo json_encode($lat); ?>; </script>
+<script type="text/javascript"> var lng[] =<?php echo json_encode($lng); ?>; </script>
+<script src="custom.js"> </script> 
+<script src="tree.js"></script>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+<style type="text/css">
+.auto-style1 {
+	text-align: center;
+}
+.auto-style2 {
+	color: #FFFFFF;
+}
+</style>
+</head>
+
+<?php
+session_start();
+session_destroy();
+
+?>
+<body bgcolor="#2A2A44">
+<div class="auto-style1">
+<form>
+<p><span class="auto-style2"><b>You have successfully logged out</b></span></p>
+<button formaction='../index.php' style='width: 10%'>Return Home</button>
+</form>
+</div>
+
+</html>
